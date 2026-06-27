eb4236f8ae75a741c793541477fd0aa0b92e2d89
Zytronium <dastelljes@outlook.com>
2026-06-27 08:35:36 -0500

    Allow setting playback speed to 3x

--- src/components/track-player.tsx
+++ src/components/track-player.tsx
@@ -288,6 +288,7 @@ export default function TrackPlayer({ tracks }: { tracks: Track[] }) {
                         <option value={1.25}>1.25×</option>
                         <option value={1.5}>1.5×</option>
                         <option value={2}>2×</option>
+                        <option value={3}>3×</option>
                     </select>
 
                 </div>
